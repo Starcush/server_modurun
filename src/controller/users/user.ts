f80ace41a929cd435630ac3208a563e557e42add
Park seongho <parkseongho@MacBook-Pro.local>
2020-06-15 10:31:32 +0900

    - FIX : users / jwt payload fixed email => userInfo(email,userid) - ADD : messages / massage files added but, it will move to message branch

--- src/controller/users/user.ts
+++ src/controller/users/user.ts
@@ -16,17 +16,17 @@ export default {
   patch: async (req, res) => {
     const { username } = req.body;
 
-    const email: string = userUtil.jwt.verify(req.session.userToken, (err, decoded) => {
+    const userInfo = userUtil.jwt.verify(req.session.userToken, (err, decoded) => {
       if (err) return false;
       return decoded.data;
     });
-
+    console.log(userInfo);
     const response: any = await userRepository.getUserDataByUsername(username);
 
     if (response) {
       res.status(409).send('Username conflict');
     } else {
-      userRepository.updateUsernameByEmail(email, username);
+      userRepository.updateUsernameByEmail(userInfo.email, username);
       res.status(200).send('Username updated');
     }
   },

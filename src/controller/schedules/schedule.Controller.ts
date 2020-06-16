05fcfdb47ad31eb8f168c9b43869005831ab08e1
lmt <1571min@naver.com>
2020-06-16 16:47:56 +0900

    MODIFY|ADD: refactoring file name and schedule router * rename controller     * chanege ...Urls files to track.controller.ts * schedule router     * imeplement schedule router using contoller

---
+++ src/controller/schedules/schedule.Controller.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import { Request, Response } from 'express';
+
+export default {
+  post: async (req: Request, res: Response) => {
+    res.send(200);
+  },
+  get: async (req: Request, res: Response) => {
+    res.send(200);
+  },
+};
